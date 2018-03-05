1fc638da3867008124a914ab77dad502e5bf8074
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-05 17:21:27 -0500

    s3_authenticated_download

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -17,4 +17,5 @@ if ENV["S3_OMNIBUS_CACHE_BUCKET"]
   s3_role true
   s3_role_arn "arn:aws:iam::486234852809:role/ci-datadog-agent"
   s3_role_session_name ENV["ROLE_SESSION_NAME"]
+  s3_authenticated_download true
 end
